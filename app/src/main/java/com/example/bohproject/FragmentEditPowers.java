d493523dd5f3e9cdca9c4ad39cb4353a2efb5535
Blake-Collin <collinblake1@gmail.com>
2019-06-13 00:11:00 -0600

    Added some fine tuning and functions to the Edit Activity

--- app/src/main/java/com/example/bohproject/FragmentEditPowers.java
+++ app/src/main/java/com/example/bohproject/FragmentEditPowers.java
@@ -6,9 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class FragmentEditPowers extends Fragment {
+public class FragmentEditPowers extends Fragment implements OnClickListener {
 
   View view;
 
@@ -19,8 +22,47 @@ public class FragmentEditPowers extends Fragment {
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    view = inflater.inflate(R.layout.edit_powers_fragment,container, false);
+    view = inflater.inflate(R.layout.edit_powers_fragment, container, false);
+
+    Button b = (Button) view.findViewById(R.id.buttonPowerAdd);
+    b.setOnClickListener(this);
+
     return view;
   }
+
+  /**
+   * Button Switch
+   * @param v
+   */
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.buttonPowerAdd:
+        onPowerAdd();
+        break;
+      default:
+        break;
+    }
+  }
+
+  public void onPowerAdd() {
+
+    //Get EditTexts
+    EditText editTextPowerName = view.findViewById(R.id.editTextPowerName);
+    EditText editTextPowerAPs = view.findViewById(R.id.editTextPowerAPs);
+
+    //Assign Values
+    String name = editTextPowerName.getText().toString();
+    int APs = Integer.parseInt(editTextPowerAPs.getText().toString());
+
+    System.out.println("Power: " + name + "\n" + "APs: " + APs);
+
+    //Get character here and add the new power item.
+
+
+    //Clear Inputs
+    editTextPowerAPs.setText("");
+    editTextPowerName.setText("");
+  }
+
 
 }
